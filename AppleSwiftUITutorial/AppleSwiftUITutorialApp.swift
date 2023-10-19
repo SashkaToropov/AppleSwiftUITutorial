8e8fb23a9640e0456b13694e99779291b02f73b9
SashkaToropov <toropov.oleksandr03@gmail.com>
2023-10-19 23:37:52 +0300

    Initial Commit

---
+++ AppleSwiftUITutorial/AppleSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSwiftUITutorialApp.swift
+//  AppleSwiftUITutorial
+//
+//  Created by  Toropov Oleksandr on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
